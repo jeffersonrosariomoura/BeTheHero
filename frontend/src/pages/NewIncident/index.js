3b89621beb9d3efcfee64cfebda61d0af291c731
Jefferson R Moura <jefferson.rosariomoura@outlook.com>
2020-04-11 15:08:30 -0300

    Commit BeTheHero

---
+++ frontend/src/pages/NewIncident/index.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import api from '../../services/api';
+
+import './styles.css';
+
+import imageLogo from '../../assets/logo.svg';
+
+export default function NewIncident() {
+    const history = useHistory();
+
+    const ongId = localStorage.getItem('ongId');
+
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [value, setValue] = useState('');
+
+    async function handleNewIncident(e) {
+        e.preventDefault();
+
+        const data = {
+            title,
+            description,
+            value
+        };
+
+        try {
+            await api.post('incidents', data, {
+                headers: {
+                    Authorization: ongId
+                }
+            });
+
+            history.push('/profile');
+        } catch (err) {
+            alert('Erro ao cadastrar caso, tente novamente.');
+        }
+    }
+
+    return (
+        <div className="new-incident-container">
+            <div className="content">
+                <section>
+                    <img src={imageLogo} alt="Be The Hero" />
+
+                    <h1>Cadastrar novo caso</h1>
+                    <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso.</p>
+
+                    <Link className="back-link" to="/profile">
+                        <FiArrowLeft size={16} color="#E02041" />
+                        Voltar para o home
+                    </Link>
+                </section>
+
+                <form onSubmit={handleNewIncident}>
+                    <input
+                        placeholder="Título do caso"
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                    />
+                    <textarea
+                        placeholder="Descrição"
+                        value={description}
+                        onChange={e => setDescription(e.target.value)}
+                    />
+                    <input
+                        placeholder="Valor em reais"
+                        value={value}
+                        onChange={e => setValue(e.target.value)}
+                    />
+
+                    <div className="button-group">
+                        <Link to="/profile" className="cancel-link">
+                            Cancelar
+                        </Link>
+                        <button type="submit" className="button">Cadastrar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
